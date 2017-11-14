e17ef5990f106087b75470b53bf0235ee11216ad
mihail-kovashki <mihail.kovashki@yahoo.com>
2017-11-14 11:57:00 +0200

    Design Changes v.0.2

--- client/src/app/shared/page-not-found.component.ts
+++ client/src/app/shared/page-not-found.component.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 @Component({
   selector: 'not-found',
-  template: `<h1>Page not found</h1>`
+  template: `<h1 class="profile-heading">Page not found</h1>`
 })
 
 export class PageNotFoundComponent {}
